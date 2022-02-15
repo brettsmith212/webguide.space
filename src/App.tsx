6e46675929e5c22d712f69f08367a2c1bc37baa3
Brett Smith <67445684+brettsmith212@users.noreply.github.com>
2022-02-15 07:22:07 +0000

    added counter | updated title

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,16 @@ import { useState } from "react";
 function App() {
   const [count, setCount] = useState(0);
 
+  const handleClick = () => {
+    setCount(prev => prev + 1)
+  }
+
   return (
     <div className="flex justify-center">
       <div className="flex flex-col items-center">
         <h1 className="text-2xl text-blue-800">WebGuide.Space</h1>
-        <p>This is the Guide!</p>
+        <p>Count: {count}</p>
+        <button className="p-2 mt-2 border border-gray-400 rounded-md" onClick={handleClick}>+1</button>
       </div>
     </div>
   );
