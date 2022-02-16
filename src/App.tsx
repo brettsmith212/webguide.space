b52e2a8075d28f01033e41841546550726493a18
Brett Smith <brettsmith212@gmail.com>
2022-02-15 16:09:40 -0800

    base layer routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
 import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import HomePage from "./components/HomePage";
 
 function App() {
   const [count, setCount] = useState(0);
 
   const handleClick = () => {
-    setCount(prev => prev + 1)
-  }
+    setCount((prev) => prev + 1);
+  };
 
   return (
-    <div className="flex justify-center">
-      <div className="flex flex-col items-center">
-        <h1 className="text-2xl text-blue-800">WebGuide.Space</h1>
-        <p>Count: {count}</p>
-        <button className="p-2 mt-2 border border-gray-400 rounded-md" onClick={handleClick}>+1</button>
-      </div>
-    </div>
+    <Router>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div>
+      <div>
+        <NavLink to="/">WebGuide</NavLink>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
