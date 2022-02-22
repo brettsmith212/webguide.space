735ec38fba8c553f38bee05ff937c307b9822029
Brett Smith <brettsmith212@gmail.com>
2022-02-21 20:39:18 -0800

    favicon added

--- src/components/guide/resources/Resources.tsx
+++ src/components/guide/resources/Resources.tsx
@@ -54,7 +54,9 @@ const Resources: React.FC<Props> = ({ show }) => {
     const resourcesArr = filteredResources.map((resource) => (
       <a href={resource.url} target="_blank" key={resource.id}>
         <div className="flex flex-col items-center gap-4 mb-6 text-center rounded-md md:w-80 w-96 md:h-96 min-h-96 h-fit p-6 shadow-lg hover:scale-105 hover:bg-violet-50">
-          <h3 className="md:text-xl text-3xl">{resource.title}</h3>
+          <h3 className="md:text-xl text-3xl text-violet-700">
+            {resource.title}
+          </h3>
           <img
             className="h-32 rounded-md"
             src={resource.image}
